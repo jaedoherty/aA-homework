b4969432c0298aa4ccc65a4ac612e21b7023c527
Jae Doherty <jaeld97@gmail.com>
2020-10-17 23:15:21 -0700

    Filled out framework

--- w5d1_hw/LRU.rb
+++ w5d1_hw/LRU.rb
@@ -1,23 +1,34 @@
 #aA homework
 
   class LRUCache
-    def initialize
+    def initialize(n)
+        @cache = []
+        @max_length = n
     end
 
     def count
       # returns number of elements currently in cache
+      @cache.length
     end
 
     def add(el)
       # adds element to cache according to LRU principle
+        if count < max_length
+            cache << el 
+        else
+            cache.shift
+            cache << el
+        end
     end
 
     def show
       # shows the items in the cache, with the LRU item first
+      puts cache.dup
     end
 
     private
     # helper methods go here!
+    attr_reader :cache, :max_length
 
   end
 
